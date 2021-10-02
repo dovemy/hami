e2d46f0c1e6bd06a0e957274ffa8fa6c77a60ae9
许浩明 <1019103752@qq.com>
2021-10-02 17:37:45 +0800

    新增mybatis-plus模块

---
+++ hami-mybatis-plus-spring-boot-starter/src/main/java/com/xuhaoming/hami/mybatisplus/HamiMybatisPlusAutoConfiguration.java
@@ -0,0 +1,41 @@
+package com.xuhaoming.hami.mybatisplus;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MybatisPlus自动配置类
+ *
+ * @author xuhaoming
+ * @date 2021/10/1
+ */
+@Configuration
+@EnableConfigurationProperties(HamiMybatisPlusProperties.class)
+public class HamiMybatisPlusAutoConfiguration {
+
+    private final HamiMybatisPlusProperties mybatisPlusProperties;
+
+    @Autowired
+    public HamiMybatisPlusAutoConfiguration(HamiMybatisPlusProperties mybatisPlusProperties) {
+        this.mybatisPlusProperties = mybatisPlusProperties;
+    }
+
+    /**
+     * 注册拦截器
+     */
+    @Bean
+    @ConditionalOnMissingBean(MybatisPlusInterceptor.class)
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        DbType dbType = DbType.getDbType(mybatisPlusProperties.getDbType());
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(dbType));
+        return interceptor;
+    }
+
+}
---
+++ hami-mybatis-plus-spring-boot-starter/src/main/java/com/xuhaoming/hami/mybatisplus/HamiMybatisPlusProperties.java
@@ -0,0 +1,22 @@
+package com.xuhaoming.hami.mybatisplus;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * MybatisPlus配置属性
+ *
+ * @author xuhaoming
+ * @date 2021/10/1
+ */
+@Data
+@ConfigurationProperties("hami.mybatis-plus")
+public class HamiMybatisPlusProperties {
+
+    /**
+     * 数据库类型，默认用MySQL
+     * {@link com.baomidou.mybatisplus.annotation.DbType}
+     */
+    private String dbType = "mysql";
+
+}
